b68894c38d0f216aee2e01c8f0c378a4530f5daa
이민구 <mingu@MacBookPro.local>
2021-03-20 14:13:56 +0900

    CRUD with JPA

--- src/main/java/com/mingu/adminpage/model/entity/User.java
+++ src/main/java/com/mingu/adminpage/model/entity/User.java
@@ -2,12 +2,14 @@ package com.mingu.adminpage.model.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class User {
 
---
+++ src/test/java/com/mingu/adminpage/repository/UserRepositoryTest.java
@@ -0,0 +1,69 @@
+package com.mingu.adminpage.repository;
+
+import com.mingu.adminpage.AdminpageApplicationTests;
+import com.mingu.adminpage.model.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public class UserRepositoryTest extends AdminpageApplicationTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create() {
+        User user = new User();
+        user.setAccount("TestUser03");
+        user.setEmail("TestUser03@gmail.com");
+        user.setPhoneNumber("010-1111-1113");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("admin");
+
+        User newUser = userRepository.save(user);
+        System.out.println("newUser: "+newUser);
+    }
+
+    @Test
+    public void read() {
+        Optional<User> user = userRepository.findById(1L);
+
+        user.ifPresent(selectUser -> {
+            System.out.println("user: "+selectUser.getAccount());
+            System.out.println("email: "+selectUser.getEmail());
+        });
+    }
+
+    @Test
+    public void update() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("PPPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
+    }
+
+    @Test
+    public void delete() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(2L);
+
+        if (deleteUser.isPresent()) {
+            System.out.println("데이터 존재: "+deleteUser.get());
+        } else {
+            System.out.println("데이터 삭제 데이터 없음");
+        }
+    }
+}
